708d3d02786ee2156b097c93c5613c6440087f18
Saiya Din <saiya00@outlook.com>
2016-05-24 02:12:15 +0000

    Controller

---
+++ MVC/controller/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class Controller{
+    
+    protected $view;
+    
+    public function __construct(){
+        $this->view = new View();
+    }
+    
+      
+    }
+    
+
+
+?>
