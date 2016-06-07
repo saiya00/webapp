8897cc774f2922fe8d96fa97f740b0086458ba83
Saiya Din <saiya00@outlook.com>
2016-06-07 06:45:27 +0000

    pagina do usuario inserido com sucesso

---
+++ MVC/view/sucesso.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>sucesso</title>
+<meta charset="UTF-8">
+<style>
+    <?php include 'static/teste.css'; ?>
+</style>
+</head>
+<body>
+<main>
+	
+	<h1>Inserido com sucesso!</h1>
+	
+	<center>
+	<a href="https://webapp-saiyadin.c9users.io/usuario/login" >Voltar ao Login</a>
+	</center>
+	
+</main>
+
+</body>
+</html>
+<html>
