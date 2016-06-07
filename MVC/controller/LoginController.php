ce1b34a2eb4805964513f10aa93af4ae19bc6b88
Saiya Din <saiya00@outlook.com>
2016-06-07 06:39:00 +0000

    LoginController

---
+++ MVC/controller/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+class LoginController extends Controller{
+    
+    public function __call($m,$a){
+        $this->view->renderizar("erro");
+    }
+    
+    public function logout(){
+        $this->estaAutorizado();
+        unset($_SESSION["_ID"]);
+        $this->view->renderizar("logout");
+    }
+    
+    public function entrar(){
+        unset($_SESSION["_ID"]);
+        $this->view->renderizar("login");
+    }
+    
+    public function autenticar(){
+        $um = new usuarioDAO();
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+        $ehLoginCorreto = $um->authUser($email,$senha);
+        if($ehLoginCorreto === false){
+            echo"usuario e senha incorretos";
+            header("Location: /usuario/login");
+            
+        }else{
+            $_SESSION["_ID"] = $ehLoginCorreto;
+            header("Location: /usuario/perfil/" . $ehLoginCorreto);
+        }
+    }
+    
+}
