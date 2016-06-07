6e8faf68be79577077c0c2b000dd1e13705eaa9e
Saiya Din <saiya00@outlook.com>
2016-06-07 06:44:30 +0000

    pagina de erro

---
+++ MVC/view/erro.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>erro</title>
+<meta charset="UTF-8">
+<style>
+    <?php include 'static/teste.css'; ?>
+</style>
+</head>
+<body>
+<main>
+	
+	<h1>Erro 404</h1>
+	<center>
+	<a href="https://webapp-saiyadin.c9users.io/usuario/login" >Voltar ao Login</a>
+	</center>
+	
+</main>
+</body>
+</html>
+<html>
